98057280c4d08a3c677e30a0d1bb81aa30609f9d
antoinerobin <antoine.robin@vesperiagroup.com>
2015-10-01 13:59:06 +0200

    get old stored data and store them the new way

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -21,6 +21,7 @@
 
 namespace oat\taoOutcomeRds\scripts\update;
 
+use oat\taoOutcomeRds\model\RdsResultStorage;
 
 class Updater extends \common_ext_ExtensionUpdater 
 {
@@ -35,6 +36,76 @@ class Updater extends \common_ext_ExtensionUpdater
         $currentVersion = $initialVersion;
 		if ($currentVersion == '1.0' || $currentVersion == '1.0.1' || $currentVersion == '1.0.2' ) {
 			$currentVersion = '1.0.3';
+		}
+
+		if ($currentVersion == '1.0.3') {
+
+			//get variables
+			$persistence = \common_persistence_Manager::getPersistence('default');
+			$sql = 'SELECT * FROM ' . RdsResultStorage::VARIABLES_TABLENAME . '
+			ORDER BY ' . RdsResultStorage::VARIABLES_TABLE_ID;
+			$params = array();
+			$variables = $persistence->query($sql, $params);
+
+			//update variable storage table schema
+			$schema = $persistence->getDriver()->getSchemaManager()->createSchema();
+			$fromSchema = clone $schema;
+
+			$tableVariables = $schema->getTable(RdsResultStorage::VARIABLES_TABLENAME);
+			$tableVariables->addColumn(RdsResultStorage::VARIABLE_VALUE, "text", array("notnull" => false));
+			$tableVariables->dropColumn(RdsResultStorage::VARIABLE_IDENTIFIER);
+			$queries = $persistence->getPlatform()->getMigrateSchemaSql($fromSchema, $schema);
+			foreach ($queries as $query) {
+				$persistence->exec($query);
+			}
+
+			//store information the new way
+
+			foreach($variables as $variable){
+				//get Variable informations
+				$sql = 'SELECT * FROM ' . RdsResultStorage::RESULT_KEY_VALUE_TABLE_NAME . '
+				WHERE ' . RdsResultStorage::RESULTSKV_FK_COLUMN .' = ?';
+				$params = array($variable[RdsResultStorage::VARIABLES_TABLE_ID]);
+				$variableValues = $persistence->query($sql, $params);
+
+				if (class_exists($variable[RdsResultStorage::VARIABLE_CLASS])) {
+					$resultVariable = new $variable[RdsResultStorage::VARIABLE_CLASS]();
+				} else {
+					$resultVariable = new \taoResultServer_models_classes_OutcomeVariable();
+				}
+
+				foreach($variableValues as $variableValue){
+					$setter = 'set' . ucfirst($variableValue[RdsResultStorage::KEY_COLUMN]);
+					$value = $variableValue[RdsResultStorage::VALUE_COLUMN];
+					if (method_exists($resultVariable, $setter) && !is_null($value)) {
+						if ($variableValue[RdsResultStorage::KEY_COLUMN] == 'value' || $variableValue[RdsResultStorage::KEY_COLUMN] == 'candidateResponse') {
+							$value = base64_decode($value);
+						}
+
+						$resultVariable->$setter($value);
+					}
+				}
+
+				$sqlUpdate = 'UPDATE ' . RdsResultStorage::VARIABLES_TABLENAME . ' SET ' . RdsResultStorage::VARIABLE_VALUE . ' = ? WHERE ' . RdsResultStorage::VARIABLES_TABLE_ID . ' = ?';
+				$paramsUpdate = array(serialize($resultVariable), $variable[RdsResultStorage::VARIABLES_TABLE_ID]);
+				$persistence->exec($sqlUpdate, $paramsUpdate);
+
+			}
+
+			//remove kv table
+
+			$schema = $persistence->getDriver()->getSchemaManager()->createSchema();
+			$fromSchema = clone $schema;
+
+			$tableVariables = $schema->getTable(RdsResultStorage::VARIABLES_TABLENAME);
+			$resultKv = $schema->dropTable(RdsResultStorage::RESULT_KEY_VALUE_TABLE_NAME);
+			$tableVariables->dropColumn(RdsResultStorage::VARIABLE_CLASS);
+			$queries = $persistence->getPlatform()->getMigrateSchemaSql($fromSchema, $schema);
+			foreach ($queries as $query) {
+				$persistence->exec($query);
+			}
+
+			$currentVersion = '1.1.0';
 		}
 
 		return $currentVersion;
