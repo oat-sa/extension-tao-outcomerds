11bcce5df9497c44a74344db6fe83ae953a39998
antoinerobin <antoine.robin@vesperiagroup.com>
2016-08-23 10:16:13 +0200

    update version number

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -120,7 +120,7 @@ class Updater extends \common_ext_ExtensionUpdater
 		}
 		$this->setVersion($currentVersion);
 		
-		$this->skip('1.1.0','1.1.2');
-		return null;
+		$this->skip('1.1.0','1.1.3');
+
 	}
 }
