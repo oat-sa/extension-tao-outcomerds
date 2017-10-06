b2790f411fff0f940f6a25132fcfe30ddea9dd37
bugalot <jerome@taotesting.com>
2017-10-06 15:32:02 +0200

    Bump to version 3.0.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -130,7 +130,7 @@ class Updater extends \common_ext_ExtensionUpdater
             ]));
             $this->setVersion('2.2.0');
         }
-		$this->skip('2.2.0','2.2.1');
-
+        
+		$this->skip('2.2.0','3.0.0');
 	}
 }
