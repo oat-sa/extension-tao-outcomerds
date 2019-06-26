1ad766376dd8322f06824310c2280f5fdab4092b
Julien Sébire <julien@taotesting.com>
2019-06-26 13:07:22 +0200

    Increased coverage of RdsResultStorage.

--- test/unit/model/RdsResultStorageTest.php
+++ test/unit/model/RdsResultStorageTest.php
@@ -81,6 +81,171 @@ class RdsResultStorageTest extends TestCase
         $this->assertSame($deliveryIdentifier, $this->instance->getDelivery($deliveryResultIdentifier));
     }
 
+    public function testGetAllTestTakerIds()
+    {
+        $deliveryResultIdentifier = "MyDeliveryResultIdentifier#1";
+        $testTakerIdentifier = "mytestTaker#1";
+        $this->instance->storeRelatedTestTaker($deliveryResultIdentifier, $testTakerIdentifier);
+
+        $expected = [
+            [
+                'deliveryResultIdentifier' => $deliveryResultIdentifier,
+                'testTakerIdentifier' => $testTakerIdentifier,
+            ],
+        ];
+
+        $this->assertSame($expected, $this->instance->getAllTestTakerIds());
+    }
+
+    public function testGetAllDeliveryIds()
+    {
+        $deliveryResultIdentifier = "MyDeliveryResultIdentifier#1";
+        $deliveryIdentifier = "mytestTaker#1";
+        $this->instance->storeRelatedDelivery($deliveryResultIdentifier, $deliveryIdentifier);
+
+        $expected = [
+            [
+                'deliveryResultIdentifier' => $deliveryResultIdentifier,
+                'deliveryIdentifier' => $deliveryIdentifier,
+            ],
+        ];
+
+        $this->assertSame($expected, $this->instance->getAllDeliveryIds());
+    }
+
+    public function testCountResultByDelivery()
+    {
+        $deliveryResultIdentifier1 = "MyDeliveryResultIdentifier#1";
+        $deliveryResultIdentifier2 = "MyDeliveryResultIdentifier#2";
+        $testTakerIdentifier1 = "mytestTaker#1";
+        $testTakerIdentifier2 = "mytestTaker#2";
+        $deliveryIdentifier1 = "myDelivery#1";
+        $deliveryIdentifier2 = "myDelivery#2";
+
+        $this->instance->storeRelatedTestTaker($deliveryResultIdentifier1, $testTakerIdentifier1);
+        $this->instance->storeRelatedDelivery($deliveryResultIdentifier1, $deliveryIdentifier1);
+        $this->instance->storeRelatedTestTaker($deliveryResultIdentifier2, $testTakerIdentifier2);
+        $this->instance->storeRelatedDelivery($deliveryResultIdentifier2, $deliveryIdentifier2);
+
+        $this->assertEquals(2, $this->instance->countResultByDelivery([]));
+        $this->assertEquals(1, $this->instance->countResultByDelivery([$deliveryIdentifier1]));
+    }
+
+    /**
+     * @dataProvider resultByDeliveryToTest
+     *
+     * @param $ids
+     * @param $selected
+     * @param $options
+     * @param $expected
+     */
+    public function testGetResultByDelivery($ids, $selected, $options, $expected)
+    {
+        $this->instance->storeRelatedTestTaker($ids['dr11'], $ids['tt1']);
+        $this->instance->storeRelatedDelivery($ids['dr11'], $ids['d1']);
+        $this->instance->storeRelatedTestTaker($ids['dr12'], $ids['tt2']);
+        $this->instance->storeRelatedDelivery($ids['dr12'], $ids['d1']);
+        $this->instance->storeRelatedTestTaker($ids['dr21'], $ids['tt1']);
+        $this->instance->storeRelatedDelivery($ids['dr21'], $ids['d2']);
+        $this->instance->storeRelatedTestTaker($ids['dr22'], $ids['tt2']);
+        $this->instance->storeRelatedDelivery($ids['dr22'], $ids['d2']);
+
+        foreach ($expected as &$fields) {
+            $fields = [
+                'deliveryResultIdentifier' => $ids[$fields[0]],
+                'testTakerIdentifier' => $ids[$fields[1]],
+                'deliveryIdentifier' => $ids[$fields[2]],
+            ];
+        }
+
+        $this->assertEquals($expected, $this->instance->getResultByDelivery($selected, $options));
+    }
+
+    public function resultByDeliveryToTest()
+    {
+        $ids = [
+            'dr11' => 'MyDeliveryResultIdentifier#11',
+            'dr12' => 'MyDeliveryResultIdentifier#12',
+            'dr21' => 'MyDeliveryResultIdentifier#21',
+            'dr22' => 'MyDeliveryResultIdentifier#22',
+            'tt1' => 'mytestTaker#1',
+            'tt2' => 'mytestTaker#2',
+            'd1' => 'myDelivery#1',
+            'd2' => 'myDelivery#2',
+        ];
+
+        return [
+            'all deliveries' => [
+                $ids,
+                [],
+                [],
+                [
+                    ['dr11', 'tt1', 'd1'],
+                    ['dr12', 'tt2', 'd1'],
+                    ['dr21', 'tt1', 'd2'],
+                    ['dr22', 'tt2', 'd2'],
+                ],
+            ],
+            'delivery1' => [
+                $ids,
+                [$ids['d1']],
+                ['order' => RdsResultStorage::DELIVERY_COLUMN],
+                [
+                    ['dr11', 'tt1', 'd1'],
+                    ['dr12', 'tt2', 'd1'],
+                ],
+            ],
+            'delivery1+2 by testtaker desc' => [
+                $ids,
+                [$ids['d1'], $ids['d2']],
+                ['order' => RdsResultStorage::TEST_TAKER_COLUMN, 'orderdir' => 'desc'],
+                [
+                    ['dr12', 'tt2', 'd1'],
+                    ['dr22', 'tt2', 'd2'],
+                    ['dr11', 'tt1', 'd1'],
+                    ['dr21', 'tt1', 'd2'],
+                ],
+            ],
+            'limit + offset' => [
+                $ids,
+                [],
+                ['order' => RdsResultStorage::RESULTS_TABLE_ID, 'limit' => 2, 'offset' => 1],
+                [
+                    ['dr12', 'tt2', 'd1'],
+                    ['dr21', 'tt1', 'd2'],
+                ],
+            ],
+            'not existing delivery' => [
+                $ids,
+                ['not existing delivery'],
+                [],
+                [],
+            ],
+        ];
+    }
+
+    public function testDeleteResult()
+    {
+        $deliveryResultIdentifier1 = "MyDeliveryResultIdentifier#1";
+        $deliveryResultIdentifier2 = "MyDeliveryResultIdentifier#2";
+        $testTakerIdentifier1 = "mytestTaker#1";
+        $testTakerIdentifier2 = "mytestTaker#2";
+        $deliveryIdentifier1 = "myDelivery#1";
+        $deliveryIdentifier2 = "myDelivery#2";
+
+        $this->instance->storeRelatedTestTaker($deliveryResultIdentifier1, $testTakerIdentifier1);
+        $this->instance->storeRelatedDelivery($deliveryResultIdentifier1, $deliveryIdentifier1);
+        $this->instance->storeRelatedTestTaker($deliveryResultIdentifier2, $testTakerIdentifier2);
+        $this->instance->storeRelatedDelivery($deliveryResultIdentifier2, $deliveryIdentifier2);
+
+        $this->assertEquals(2, $this->instance->countResultByDelivery([]));
+        $this->assertTrue($this->instance->deleteResult($deliveryResultIdentifier1));
+        $this->assertEquals(1, $this->instance->countResultByDelivery([]));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Variables storing
+
     public function testStoreItemVariable()
     {
         $deliveryResultIdentifier = "MyDeliveryResultIdentifier#1";
