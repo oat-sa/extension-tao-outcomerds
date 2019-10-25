24c802dee057d0e233822a563d61b69649deb689
Mikhail Kamarouski <kamarouski@1pt.com>
2019-10-25 16:32:24 +0300

    NewRdsStorage - as default for variables

--- config/default/RdsResultStorage.conf.php
+++ config/default/RdsResultStorage.conf.php
@@ -4,8 +4,9 @@
  *
  * To replace this add a file /home/bout/code/php/taoTrunk/taoResultServer/config/header/default_resultserver.conf.php
  */
-use oat\taoOutcomeRds\model\RdsResultStorage;
 
-return new RdsResultStorage([
-    RdsResultStorage::OPTION_PERSISTENCE => 'default',
+use oat\taoOutcomeRds\model\NewSqlResultStorage;
+
+return new NewSqlResultStorage([
+    NewSqlResultStorage::OPTION_PERSISTENCE => 'default',
 ]);
