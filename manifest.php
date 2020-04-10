76868d8bf4bd4bec9c74e475fb5ebc4ee006d2fd
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-04-10 17:19:04 +0300

    remove taoResultServer_models_classes_ResultServerAuthoringService

--- manifest.php
+++ manifest.php
@@ -25,7 +25,7 @@ return [
     'label' => 'extension-tao-outcomerds',
     'description' => 'extension that allows a storage in relational database',
     'license' => 'GPL-2.0',
-    'version' => '7.0.0',
+    'version' => '7.1.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'taoResultServer' => '>=11.0.0',
@@ -42,7 +42,6 @@ return [
         ],
         'php' => [
             \oat\taoOutcomeRds\scripts\install\CreateTables::class,
-            dirname(__FILE__) . '/scripts/install/setDefault.php',
         ]
     ],
     'uninstall' => [
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -163,6 +163,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('6.1.0');
         }
 
-        $this->skip('6.1.0', '7.0.0');
+        $this->skip('6.1.0', '7.1.0');
     }
 }
