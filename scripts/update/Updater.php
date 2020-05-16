86c0a698b4ff08bc6c14cd9d250e8d6bf5440b5f
bugalot <bugalot1985@gmail.com>
2020-05-16 19:13:54 +0200

    Bump version 7.1.1

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -164,5 +164,11 @@ class Updater extends \common_ext_ExtensionUpdater
         }
 
         $this->skip('6.1.0', '7.1.0');
+
+        if ($this->isVersion('7.1.0')) {
+
+            $this->getLogger()->debug('taoOutcomeRds update!');
+            $this->setVersion('7.1.1');
+        }
     }
 }
